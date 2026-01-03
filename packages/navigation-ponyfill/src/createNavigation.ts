c2594ec0464b25d4ffbe268915abc513a0e46de3
Kevin Crawford <kcrwfrd.dev@gmail.com>
2026-01-02 16:42:48 -0800

    feat: defer to native Navigation when available

--- packages/navigation-ponyfill/src/createNavigation.ts
+++ packages/navigation-ponyfill/src/createNavigation.ts
@@ -1,10 +1,15 @@
 import { Navigation } from './Navigation'
 import { HistoryShim } from './HistoryShim'
 
+type NativeNavigation = Window['navigation']
+
 export function createNavigation(
   history: History | HistoryShim = typeof window !== 'undefined'
     ? window.history
     : new HistoryShim(),
-) {
+): Navigation | NativeNavigation {
+  if (typeof window !== 'undefined' && 'navigation' in window) {
+    return window.navigation
+  }
   return new Navigation(history)
 }
