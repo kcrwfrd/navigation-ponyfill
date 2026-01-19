8d343545df01e6068cf34f1860d0c25791c3a96e
Kevin Crawford <kcrwfrd.dev@gmail.com>
2026-01-18 22:11:51 -0800

    test(e2e): use next js prod build during CI

--- examples/next/playwright.config.ts
+++ examples/next/playwright.config.ts
@@ -72,9 +72,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: process.env.CI ? 'npm run build && npm run start' : 'npm run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000, // Next.js compile time
+    timeout: 120 * 1000,
   },
 })
